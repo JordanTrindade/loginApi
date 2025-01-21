541cc94a996622817f685cb705f67d5e1d503cd7
JordanTrindade <jordandimst@gmail.com>
2025-01-21 18:22:38 -0300

    Create UsarioDetailsImplements.java

---
+++ api/src/main/java/com/login/api/security/userDetails/UsarioDetailsImplements.java
@@ -0,0 +1,60 @@
+package com.login.api.security.userDetails;
+
+import com.login.api.entity.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class UsarioDetailsImplements implements UserDetails {
+    private final Usuario user;
+
+    public UsarioDetailsImplements(Usuario user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Stream.of(user.getUserRoles())
+                 .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return  user.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
