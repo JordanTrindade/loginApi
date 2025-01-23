8cfa32d4733ab352dc99dd5ff69926e65998f24e
JordanTrindade <jordandimst@gmail.com>
2025-01-22 21:22:51 -0300

    Update UserSecurityFilter.java

--- api/src/main/java/com/login/api/security/config/UserSecurityFilter.java
+++ api/src/main/java/com/login/api/security/config/UserSecurityFilter.java
@@ -8,11 +8,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Component
 public class UserSecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(isEndPointBloqueado(request)){
+            String token = recoverToken(request);
+        }
+    }
 
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+    private Boolean isEndPointBloqueado(HttpServletRequest request){
+        var endpoint = request.getRequestURI();
+        return !Arrays.stream(SecurityConfig.ENDPOINTS_NO_REQUIRED_AUTH).toList().contains(endpoint);
     }
 }
