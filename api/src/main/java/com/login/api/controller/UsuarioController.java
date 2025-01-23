e3972e9f1f19946d5f295c0cec483b11872acd79
JordanTrindade <jordandimst@gmail.com>
2025-01-22 21:22:46 -0300

    Create UsuarioController.java

---
+++ api/src/main/java/com/login/api/controller/UsuarioController.java
@@ -0,0 +1,9 @@
+package com.login.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "usuarios")
+public class UsuarioController {
+}
